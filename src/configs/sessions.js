6fccc0b5d1402e08ce6ccce805386f93b87051f2
Javier Emperador <javier_emperador@outlook.com>
2024-03-30 19:11:13 -0300

    Se resuelve desafío clase 10 (#19)
     - Se agrega connect-mongo como dependencia de desarrollo para poder utilizar mongodb y almacenar sesiones.
     - Se agrega express-session como dependencia de desarrollo para el manejo de sesiones .
     - Se agrega configuración para sessions en configs/sessions.js.
     - Se agrega user.model para guarda en mongodb los datos del usuario.
     - Se agregan rutas para manejo de sesiones en routes/index.
     - Se agrega en routes/mongoDB/sessions.routes.js para el manejo de las rutas de sesiones en el back con mongo como base de datos.
     - Se agrega en routes/views/sessions.view.js para el manejo de rutas de sesiones en el front (principalmente renderizado de nuevas vistas).
     - Se agregan vistas para login, register y profile con handlebars como tambien los partials para form-login, form-register y user-data.
     - Correcciones menores.

---
+++ src/configs/sessions.js
@@ -0,0 +1,20 @@
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { mongodb } from "../services/mongo.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const secret = process.env.SECRET;
+
+const sessions = session({
+  secret: secret,
+  resave: true,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: `mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PASS}@${process.env.DB_MONGO_CLUSTER}.6hohxmy.mongodb.net/${process.env.DB_MONGO_DB}?retryWrites=true&w=majority`,
+  }),
+  ttl: 100,
+});
+
+export default sessions;
---
+++ src/routes/mongoDB/sessions.routes.js
@@ -0,0 +1,41 @@
+import { UserModel } from "../../dao/models/user.model.js"
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/v1/sesssion/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email, password });
+
+  if (!user) return res.redirect("/");
+
+  req.session.user = user;
+
+  return res.redirect("/products/realtimeproducts");
+});
+
+router.post("/v1/sesssion/register", async (req, res) => {
+  const user = req.body;
+
+  if (
+    user.email === "adminCoder@coder.com" &&
+    user.password === "adminCod3r123"
+  ) {
+    user.rol = "admin";
+  }
+
+  await UserModel.create(user);
+
+  return res.redirect("/");
+});
+
+router.get("/v1/sesssion/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.json({ message: err });
+    }
+    res.redirect("/");
+  });
+});
+
+export default router;
