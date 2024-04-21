2029dcdeb99156905b72a8ebad628357afcedb78
JEmperador <javier_emperador@outlook.com>
2024-04-21 20:32:13 -0300

    Se desactiva session

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
-app.use(sessions);
+//app.use(sessions);
 
 app.use("/static", express.static("./src/public"));
 app.engine("handlebars", hbs.engine);
@@ -25,7 +25,7 @@ app.set("views", "./src/views");
 
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.session());
 initializePassport();
 
 router(app);
